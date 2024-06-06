d98ddbe3d7b3dccde2f22c6b7e71eea0fc73e236
Rakhmanin Aleksandr <a.rachmanin@yandex.ru>
2024-06-06 16:51:23 +0600

    Создал проект, подготовил к работе, создал компонент toolbar отвечающий за header страницы

---
+++ src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,11 @@
+const Toolbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success mb-4">
+            <div className="container-fluid">
+                <span className="navbar-brand ms-auto">Rakhmanin Aleksandr - Homework-57</span>
+            </div>
+        </nav>
+    );
+};
+
+export default Toolbar;
